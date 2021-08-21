38baf4d5076e5e0e25fde8f53f62acf609174aaf
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2021-08-21 10:42:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function solve(iteams)
                         iteams[i][j] =c;
                         document.getElementById(i+""+j).value = iteams[i][j]; // passing values to the document
                         document.getElementById(i+""+j).style.color = "#ff0000";//changing colour
-                        if(flag > 7290){
+                        if(flag > 72900){
                             window.alert("Invalid input provided")
                             return true
                         }
